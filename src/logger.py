11b611611d2d9744f170c2d8a6ecba5df9ebd2f7
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-05 23:31:56 +0300

    Updated logger

--- src/logger.py
+++ src/logger.py
@@ -1,16 +1,5 @@
 import logging
 
-
-class PrefixFilter(logging.Filter):
-    def __init__(self, prefix: str):
-        super().__init__()
-        self.prefix = prefix
-
-    def filter(self, record: logging.LogRecord):
-        record.msg = f"[{self.prefix}] {record.msg}"
-        return True
-
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
