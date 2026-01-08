e780f45cecfd941b46ae64721f861c175a85ae69
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-08 22:02:09 +0300

    Updated entry point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from src.service.updater import update_markets
+from src.service.markets.updater import update_markets
 
 if __name__ == "__main__":
     asyncio.run(update_markets())
