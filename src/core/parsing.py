c8a693253201606f618014ad577f58733b927486
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-09 22:32:51 +0300

    Decomposed parser classes

--- src/core/parsing.py
+++ src/core/parsing.py
@@ -1,11 +1,22 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
+from typing import Any
 
 import pandas as pd
 
 
 class BaseParser(ABC):
     def __init__(self) -> None:
+        self.file: Any = None
+
+    @abstractmethod
+    async def parse(self, file: Path) -> None:
+        pass
+
+
+class DataFrameParser(BaseParser):
+    def __init__(self) -> None:
+        super().__init__()
         self.df: pd.DataFrame = pd.DataFrame()
 
     @abstractmethod
