2e072d98e53a2a3f9092e16c44bc2af09c54e790
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-09 22:32:18 +0300

    Decomposed loader classes

--- src/core/loading.py
+++ src/core/loading.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Any, cast
 
 import pandas as pd
@@ -8,13 +9,29 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.models import BaseModel
 
 
-class BaseLoader:
+class BaseLoader(ABC):
+    def __init__(self) -> None:
+        self.rowcount: int = 0
+
+    @abstractmethod
+    async def load(self, data: Any) -> None:
+        pass
+
+
+class SQLAlchemyLoader(BaseLoader):
     def __init__(self, alchemy_model: type[BaseModel], session: AsyncSession) -> None:
+        super().__init__()
         self.model = alchemy_model
         self.session = session
-        self.rowcount = 0
 
-    async def load_df_to_db(self, df: pd.DataFrame) -> None:
+    @abstractmethod
+    async def load(self, data: Any) -> None:
+        pass
+
+
+class DataFrameLoader(SQLAlchemyLoader):
+    async def load(self, data: pd.DataFrame) -> None:
+        df = data
         df = df.convert_dtypes()  # convert data to optimal nullable Pandas types
         df = df.where(pd.notna(df), None)  # replace nullable Pandas types with Python None
         records = df.to_dict(orient="records")
