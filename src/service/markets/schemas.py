0f38c50fb37396cfd3540b92220d084513a8a17f
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-12 00:55:26 +0300

    Updated field validation

--- src/service/markets/schemas.py
+++ src/service/markets/schemas.py
@@ -1,5 +1,7 @@
+import contextlib
 import json
 from datetime import datetime
+from typing import Any
 
 from pydantic import Field, model_validator
 
@@ -12,9 +14,9 @@ class NBAMarketSchema(BaseJSONSchema):
     event_id: int | None = None
 
     market_id: int = Field(alias="id")
-    market_type: str | None = Field(alias="sportsMarketType")
+    market_type: str | None = Field(default="moneyline", alias="sportsMarketType")
     market_start: datetime = Field(alias="gameStartTime")
-    market_end: datetime | None = Field(default=None, alias="closedTime")
+    market_end: datetime = Field(alias="closedTime")
 
     order_min_price: float | None = Field(default=None, alias="orderPriceMinTickSize")
     order_min_size: float | None = Field(default=None, alias="orderMinSize")
@@ -22,16 +24,32 @@ class NBAMarketSchema(BaseJSONSchema):
     token_id_guest: str
     token_id_host: str
 
-    @model_validator(mode="after")
-    def parse_tokens(self):
-        raw_tokens = getattr(self, "clobTokenIds", None)
-        if not raw_tokens:
-            return self
+    @model_validator(mode="before")
+    def parse_dates(cls, values: dict[str, Any]):
+        date_field_names = ["gameStartTime", "closedTime"]
 
-        tokens = json.loads(raw_tokens)
-        if len(tokens) < 2:
-            raise ValueError("Cannot parse CLOB token ids")
+        for field in date_field_names:
+            val = values.get(field)
+            if isinstance(val, str):
+                if val.endswith("+00"):
+                    val = val.replace("+00", "+00:00")
+                try:
+                    values[field] = datetime.fromisoformat(val)
+                except ValueError:
+                    contextlib.suppress(ValueError)
 
-        self.token_id_guest = tokens[0]
-        self.token_id_host = tokens[1]
-        return self
+        return values
+
+    @model_validator(mode="before")
+    def parse_tokens(cls, values: Any):
+        raw_tokens = values.get("clobTokenIds")
+        if raw_tokens:
+            try:
+                tokens = json.loads(raw_tokens)
+                if len(tokens) == 2:
+                    values["token_id_guest"] = tokens[0]
+                    values["token_id_host"] = tokens[1]
+            except json.JSONDecodeError:
+                values["token_id_guest"] = None
+                values["token_id_host"] = None
+        return values
