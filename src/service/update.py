53ba65a3e2ac4ec5f230376ae101f363e66cf58b
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-11 22:36:33 +0300

    Added price updating

--- src/service/update.py
+++ src/service/update.py
@@ -1,7 +1,9 @@
 from src.service.games.updater import update_games
 from src.service.markets.updater import update_markets
+from src.service.prices.updater import update_prices
 
 
 async def run_update():
     await update_games()
     await update_markets()
+    await update_prices()
