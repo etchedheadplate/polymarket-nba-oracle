f3ca2a65fe5e230b11665d8a5f633764a33839c1
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-21 12:57:14 +0300

    Fixed file_rewrite for market client

---
+++ src/service/clients.py
@@ -0,0 +1,44 @@
+from typing import Any
+from urllib.parse import urljoin
+
+import aiohttp
+
+from src.core.dump import PolymarketClobAPIClient, PolymarketGammaAPIClient
+
+
+class NBAGamesClient(PolymarketGammaAPIClient):
+    _subdirname = "games"
+    _file_prefix = "series"
+    _file_rewrite = True
+    _rate_limit = 349  # https://docs.polymarket.com/quickstart/introduction/rate-limits#gamma-api-rate-limits
+
+    def __init__(self, session: aiohttp.ClientSession, by_slug: bool = False) -> None:
+        if by_slug:  # legacy pre-2025/26 seasons
+            super().__init__(session=session, params=[{"slug": "nba"}])
+        else:  # seasons 2025/26 and later
+            super().__init__(session=session, endpoints=["10345"])
+        self._base = urljoin(self._base, "series/")
+
+
+class NBAMarketsClient(PolymarketGammaAPIClient):
+    _subdirname = "markets"
+    _file_prefix = "event"
+    _file_rewrite = True
+    _rate_limit = 499  # https://docs.polymarket.com/quickstart/introduction/rate-limits#gamma-api-rate-limits
+
+    def __init__(self, session: aiohttp.ClientSession, endpoints: list[str]) -> None:
+        super().__init__(session=session, endpoints=endpoints)
+        self._base = urljoin(self._base, "events/")
+
+
+class NBAPricesClient(PolymarketClobAPIClient):
+    _subdirname = "prices"
+    _file_prefix = "token"
+    _file_rewrite = False
+    _rate_limit = 999  # https://docs.polymarket.com/quickstart/introduction/rate-limits#clob-markets-&-pricing
+
+    def __init__(self, session: aiohttp.ClientSession, params: list[dict[str, Any]] | None = None) -> None:
+        if params is None:
+            params = [{}]
+        super().__init__(session=session, params=params)
+        self._base = urljoin(self._base, "prices-history/")
