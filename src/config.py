8004b99e2736a7adf0a8b0d56e207bb288c008a4
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-02-22 19:49:15 +0300

    Updated config

--- src/config.py
+++ src/config.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    LOG_DIR: Path
+
     DB_NAME: str
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
+
+    REDIS_BROKER: str
+    REDIS_BACKEND: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
