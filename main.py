d852118a95214f90be161e64f89f0c79e57cbb5f
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-03-01 13:08:23 +0300

    Updated entry point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextlib
 import signal
 from typing import Any
 
@@ -11,30 +12,41 @@ from src.service.tasks import construct_game_schedule
 
 async def main():
     connection = RabbitMQConnection()
-    consumer = RabbitMQConsumer(connection, service_name="nba-oracle")
+    consumer = RabbitMQConsumer(connection, service_name=settings.SERVICE_NAME)
     producer = RabbitMQProducer(connection)
 
+    stop_event = asyncio.Event()
+
     async def handle_message(msg: dict[str, Any]):
-        schedule_range = msg["schedule_range"]
-        result = await construct_game_schedule(schedule_range)
+        result = await construct_game_schedule(msg["schedule_range"])
         await producer.send_message(
-            exchange_name=settings.EXCHANGE_NAME, routing_key=settings.RK_ORACLE_RESPONSE, message=result
+            settings.EXCHANGE_NAME,
+            settings.RK_ORACLE_RESPONSE,
+            result,
         )
 
-    await consumer.consume(
-        exchange_name=settings.EXCHANGE_NAME, routing_key=settings.RK_ORACLE_QUERY, callback=handle_message
-    )
+    async def start_consumer():
+        with contextlib.suppress(asyncio.CancelledError):
+            await consumer.consume(
+                settings.EXCHANGE_NAME,
+                settings.RK_ORACLE_QUERY,
+                handle_message,
+            )
 
-    stop_event = asyncio.Event()
     loop = asyncio.get_running_loop()
     for sig in (signal.SIGINT, signal.SIGTERM):
         loop.add_signal_handler(sig, stop_event.set)
 
-    task = asyncio.create_task(consumer.consume(settings.EXCHANGE_NAME, settings.RK_ORACLE_QUERY, handle_message))
+    tasks = [
+        asyncio.create_task(start_consumer()),
+    ]
 
     await stop_event.wait()
-    task.cancel()
-    await asyncio.gather(task, return_exceptions=True)
+
+    for task in tasks:
+        task.cancel()
+
+    await asyncio.gather(*tasks, return_exceptions=True)
     await connection.close()
 
 
