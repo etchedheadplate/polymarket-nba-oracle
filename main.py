8379a781eb0012bc1a42acc5a140db1bd222c5ba
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-03-03 00:11:13 +0300

    Updated entry point

--- main.py
+++ main.py
@@ -4,42 +4,39 @@ import signal
 from typing import Any
 
 from src.config import settings
-from src.queue.connection import RabbitMQConnection
-from src.queue.consumer import RabbitMQConsumer
-from src.queue.producer import RabbitMQProducer
-from src.service.tasks import construct_game_schedule
+from src.queue import RabbitMQConnection, RabbitMQConsumer, RabbitMQProducer
+from src.workers import Handler, Request
 
 
 async def main():
     connection = RabbitMQConnection()
-    consumer = RabbitMQConsumer(connection, service_name=settings.SERVICE_NAME)
+    consumer = RabbitMQConsumer(connection)
     producer = RabbitMQProducer(connection)
 
     stop_event = asyncio.Event()
 
-    async def handle_message(msg: dict[str, Any]):
-        result = await construct_game_schedule(msg["schedule_range"])
+    async def handle_message(msg: dict[str, Any]) -> None:
+        message = Request(**msg)
+        response = await Handler().process(message)
         await producer.send_message(
-            settings.EXCHANGE_NAME,
-            settings.RK_ORACLE_RESPONSE,
-            result,
+            exchange_name=settings.EXCHANGE_NAME,
+            routing_key=f"{settings.QUEUE_TGBOT}.{settings.RK_RESPONSE}",
+            message=response.model_dump(),
         )
 
     async def start_consumer():
         with contextlib.suppress(asyncio.CancelledError):
             await consumer.consume(
-                settings.EXCHANGE_NAME,
-                settings.RK_ORACLE_QUERY,
-                handle_message,
+                exchange_name=settings.EXCHANGE_NAME,
+                routing_key=f"{settings.QUEUE_ORACLE}.{settings.RK_REQUEST}",
+                callback=handle_message,
             )
 
     loop = asyncio.get_running_loop()
     for sig in (signal.SIGINT, signal.SIGTERM):
         loop.add_signal_handler(sig, stop_event.set)
 
-    tasks = [
-        asyncio.create_task(start_consumer()),
-    ]
+    tasks = [asyncio.create_task(start_consumer())]
 
     await stop_event.wait()
 
